1ffb051e0c81d55bf7c929174902a1f2131468b8
Tanja <tanja120a@gmail.com>
2021-10-13 19:34:38 +0200

    Validate input str and transform  str into arrays

--- js/index.js
+++ js/index.js
@@ -33,105 +33,137 @@ switcherContainer.addEventListener("click",changeTheme);
 
 //Calculator 
 
-//const validInput = /^[-+]?[0-9]+([-+*/]+[-+]?[0-9]+)*$/;
+//TODO const validInput = /^[-+]?[0-9]+([-+*/]+[-+]?[0-9]+)*$/;
 
 const inputField = document.querySelector("input");
 const btns = document.querySelectorAll(".calculator__btn");
 const displayMemory = document.querySelector(".display__memory");
 //const btnsValues = [7,8,9,"del", 4, 5, 6, "+", 1, 2, 3, "-", ".", 0, "/", "x", "reset", "=" ];
-//console.log(btns[0]);
+
 let memory = "0";
-let lastIsNumber =true;
 let lastCharacter = "0";
+let lastIsNumber =true;
 let noDots = true;
 let result = null;
+let deletedCharacter = "";
 function getCharacter(btn){
-    //(result)? console.log("ima result"): console.log("nema")
     if(btn === "del"){
-        result = null;
-        lastCharacter = memory[memory.length - 1]
-        lastIsNumber = (lastCharacter*1 || lastCharacter === "0")? true: false;
-        if(lastCharacter === ",")noDots = true;
+        result = null;        
+        lastCharacter = memory[memory.length - 2];
+        lastIsNumber = isCharacterNumber(lastCharacter);  
+        deletedCharacter = memory[memory.length -1];
         memory = memory.slice(0,memory.length-1);
+        let arrayOfDots = memory.split(",");    
+        if(arrayOfDots.length > 1) noDots = (!isNaN(arrayOfDots[arrayOfDots.length-1]*1) 
+        || lastCharacter === "," || lastCharacter === ".")?false : true;    
+        if(deletedCharacter === ",") noDots = true;
         inputField.value = memory;
-        return memory
+        return memory        
     }
     if(btn === "reset"){
+        memory = "0";
         lastCharacter = "0";
         lastIsNumber = true;
         noDots = true;
-        memory = "0";
         result = null;
         inputField.value = memory; 
         displayMemory.innerHTML = memory;       
         return memory;
     }
-    //Find is new Character Number
-    let newIsNumber = (btn*1 || btn === "0" )? true: false;
+    
+    let newIsNumber = isCharacterNumber(btn);   
     
     
-    if(newIsNumber){        
-       /* if(!result && memory !== "0"){
-        }else{
-            memory = "";
-            result = null;
-        }  */
+    if(newIsNumber){  
         if(result || memory == "0"){
             memory = "";
             result = null;
         }
-        memory += btn;
-       inputField.value = memory;  
+        memory += btn;         
     }else{
-        console.log("da li je last number = " + lastIsNumber)
         result = null;
         switch(btn){           
             case "-":
+                console.log(lastCharacter)
                 memory = (lastCharacter == "-")? memory: 
-                (lastCharacter == "+")? (memory.slice(0, memory.length-1) + btn)  : (memory + btn);
+                (lastCharacter == "+" || lastCharacter == "." ||lastCharacter =="," || memory == "0")
+                ? (memory.slice(0, memory.length-1) + btn)  : (memory + btn);
                 noDots = true;
             break;           
             case "=": 
                 (lastIsNumber)&& calculate() ;
             break;
-            case ".":
-                console.log("when dot last character is " + lastIsNumber)
+            case ".":                
                 memory = (lastIsNumber && noDots)? (memory + ","): (noDots)? memory + "0,": memory;
                 noDots = false;
             break;           
             default:
                 memory = (lastIsNumber)? (memory + btn): (memory.slice(0, memory.length-1) + btn);
                 noDots = true;
-                break;
-            /* case "+": 
-                memory = (lastIsNumber)? (memory + btn): (memory.slice(0, memory.length-1) + btn);
-                noDots = true;
-            break;
-            case "x":
-                memory = (lastIsNumber)? (memory + btn) : (memory.slice(0, memory.length-1) + btn);
-                noDots = true;
-            break;
-            case "/":
-                memory = (lastIsNumber)? (memory + btn) : (memory.slice(0, memory.length-1) + btn);
-                noDots = true;
-            break;*/    
+                break;  
         }
-        inputField.value = memory;
-    }
+    }    
+//TODO
     function calculate() {
-        console.log("calculating")
         result = memory.split("");
+        
+        console.log(result);  
+        let first = "";
+        let element = "";
+        let arrayOfNumbers = [];
+        let arrayOfOperators = [];
+        let tempLast = "";
+        //Find if the first element is -
+            first = result.shift();
+            console.log("first= " +first)
+            if(first !== "-"){
+                element = first;
+                first = "";
+            } 
+        console.log(result)
+        //Separate numbers and operators in two arrays
+        while(result.length > 0){
+            let temp = result.shift();
+            if(!isNaN(temp*1) || temp === "," ){
+                if(element === ""){
+                    element = temp;
+                }else{
+                    element += temp;
+                }                
+                tempLast = "";
+            }else{
+                if(temp === "/" || temp === "x") tempLast = temp;
+                console.log(element)
+                if(element) arrayOfNumbers.push(parseFloat(element.replace(",",".")));
+                element ="";
+                if(temp === "-" && tempLast !== ""){
+                    arrayOfOperators.pop();
+                    temp = tempLast + temp;
+                    tempLast = "";
+                }
+                arrayOfOperators.push(temp);
+            }
+        }
+        if(element !== "")arrayOfNumbers.push(parseFloat(element.replace(",",".")));
+        element ="";
+        
+        // Make calculation
+        console.log(first)
+        console.log(arrayOfOperators);
+        console.log(arrayOfNumbers);
+        console.log(element);
         memory = result[0]+"";
         btn = memory;
-        //lastIsNumber = true;
         noDots = true;
         displayMemory.innerHTML = inputField.value
         inputField.value = memory;
-        //return memory;
     }
+    function isCharacterNumber(value){
+        return (value*1 || value === "0" )? true: false;
+    }
+
+    inputField.value = memory;
     lastCharacter = btn;
-    //console.log("last character = " +lastCharacter)
-    lastIsNumber = (lastCharacter*1  || lastCharacter === "0")? true: false;;    
-    //console.log(lastIsNumber)
+    lastIsNumber = isCharacterNumber(lastCharacter);
 }
-btns.forEach(btn => btn.addEventListener("click",() => getCharacter(btn.innerHTML)/*alert(typeof(btn.innerHTML))*/))
+btns.forEach(btn => btn.addEventListener("click",() => getCharacter(btn.innerHTML)))
