ef779d4484aecb0dff58654c940313357e64be0e
Tanja <tanja120a@gmail.com>
2021-10-14 12:22:29 +0200

    Second version finished

--- js/index.js
+++ js/index.js
@@ -43,19 +43,134 @@ const btns = document.querySelectorAll(".calculator__btn");
 const displayMemory = document.querySelector(".display__memory");
 //const btnsValues = [7,8,9,"del", 4, 5, 6, "+", 1, 2, 3, "-", ".", 0, "/", "x", "reset", "=" ];
 
-/**Start Validation of Values */
+/**Start Validation of Expression */
 
 let memory = "0";
 let lastCharacter = "0";
 let lastIsNumber =true;
 let noDots = true;
 let result = null;
-let finalResult = null;
+//let finalResult = null;
 let deletedCharacter = "";
 
+const countOperation = (first, operator, second) => {
+    let res = 0 
+    console.log(operator)
+    console.log(first)
+    console.log(second)
+    switch(operator){
+        case "+": res = (first * 10 + second * 10) / 10;
+        break;
+        case "-": res = (first * 10 - second * 10) / 10;
+        break;
+        case "x": res = first * second;
+        break;
+        case "/": res = first / second;
+        break;
+        case "x-": res = -1 * first * second;
+        break;
+        case "/-": res =-1 * first / second;
+        break;
+        default: res = 0;
+            break;
+    }
+    console.log(res)
+    return res;
+}            
+
+const isCharacterNumber = (value) =>{
+    return (value*1 || value === "0" )? true: false;
+}
+
+const convertInputToValidArray = () => {
+    let firstSign = "";
+    let element = "";
+    let tempLast = "";
+    let convertedArray = [];
+    //Find if the first element is -
+    firstSign = result.shift();
+    if(firstSign !== "-"){
+        element = firstSign;
+        firstSign = "";
+    }
+
+    //Convert  array
+    while(result.length > 0){
+        let temp = result.shift();
+        if(!isNaN(temp*1) || temp === "." ){
+            /** If temp is number or "," */
+            element = (element === "")? temp: element + temp;                                
+            tempLast = "";
+        }else{
+            /**If temp is operator */
+            if(temp === "/" || temp === "x") tempLast = temp;
+            if(element) convertedArray.push(parseFloat(element));
+            element ="";
+            if(temp === "-" && tempLast !== ""){
+                convertedArray.pop();
+                temp = tempLast + temp;
+                tempLast = "";
+            }
+            convertedArray.push(temp);
+        }
+    }
+    /**Adding last element of result into convertedArray. */
+    if(element !== "")convertedArray.push(parseFloat(element));
+    element ="";
+    /**Adding minus sign on first element of convertedArray. */
+    if(firstSign !== "")convertedArray[0] = convertedArray[0] * (-1);
+    console.log(convertedArray);
+
+    // Make calculation
+    let counter = convertedArray.length
+    let newElement;
+    /**First calculate operations * and / */
+    for(let i = 0; i < convertedArray.length; i++){
+        if(convertedArray[i] == "x" || convertedArray[i] == "/" 
+        || convertedArray[i] == "x-" || convertedArray[i] == "/-"){                    
+            newElement =  countOperation(convertedArray[i-1], convertedArray[i], convertedArray[i+1])
+            convertedArray.splice(i,2);
+            convertedArray[i-1] = newElement;
+            counter = counter-2;
+            i--;
+            console.log(convertedArray)
+        }
+        if(i == counter )break ;
+    }
+    /**Second calculate operations + and - */
+    if(convertedArray.length > 1){
+        while(convertedArray.length > 1){
+           newElement = countOperation(convertedArray.shift(), convertedArray.shift(), convertedArray.shift());
+            convertedArray.unshift(newElement);
+            console.log(convertedArray)
+        }                
+    }
+    
+    return convertedArray;
+}
+
+/*Start Calculate Equal btn */
+const calculate = () => {
+    memory = memory.toString().replace(/,/g, ".");
+    result = memory.split("");        
+    /*let convertedArray = convertInputToValidArray();         
+    finalResult = parseFloat(convertedArray[0].toFixed(2));        
+    return finalResult*/
+    return parseFloat(convertInputToValidArray()[0].toFixed(2));
+}
+
 function getCharacter(btn){
+    console.log(lastCharacter);
+    console.log(btn);
+    console.log(memory)
+    let newIsNumber = isCharacterNumber(btn); 
     if(btn === "del"){
-        result = null;        
+        if (memory == NaN){
+            displayMemory.innerHTML = memory;
+            inputField.value = "0";
+            memory ="";
+        }else{
+            result = null;        
         lastCharacter = memory[memory.length - 2];
         lastIsNumber = isCharacterNumber(lastCharacter);  
         deletedCharacter = memory[memory.length -1];
@@ -65,20 +180,24 @@ function getCharacter(btn){
         || lastCharacter === "," || lastCharacter === ".")?false : true;    
         if(deletedCharacter === ",") noDots = true;
         inputField.value = memory;
-        return memory        
+        return memory  
+        }
+              
     }
     if(btn === "reset"){
-        memory = "0";
-        lastCharacter = "0";
+       // memory = "0";
+        lastCharacter = null;
         lastIsNumber = true;
         noDots = true;
         result = null;
-        inputField.value = memory; 
-        displayMemory.innerHTML = memory;       
+        //inputField.value = memory; 
+        //displayMemory.innerHTML = memory;  
+        displayMemory.innerHTML = inputField.value;  
+        inputField.value = "0";           
+        memory = "";
         return memory;
-    }
-    
-    let newIsNumber = isCharacterNumber(btn);     
+    }   
+        
     
     if(newIsNumber){  
         if(result || memory == "0"){
@@ -97,7 +216,7 @@ function getCharacter(btn){
                 noDots = true;
             break;           
             case "=": 
-                (lastIsNumber)&& calculate() ;
+                (lastIsNumber)&& displayResult() ;
             break;
             case ".":                
                 memory = (lastIsNumber && noDots)? (memory + ","): (noDots)? memory + "0,": memory;
@@ -110,112 +229,19 @@ function getCharacter(btn){
         }
     }    
 
-    /**End Validation of Values */
+    /**End Validation of Expression */    
 
-    /*Start Calculate Equal btn */
-
-    function calculate() {
-        result = memory.split("");
-        let first = "";
-        let element = "";
-        let tempLast = "";
-        //Find if the first element is -
-            first = result.shift();
-            if(first !== "-"){
-                element = first;
-                first = "";
-            }
-        //Convert  array
-        let convertedArray = [];
-        while(result.length > 0){
-            let temp = result.shift();
-            if(!isNaN(temp*1) || temp === "," ){
-                if(element === ""){
-                    element = temp;
-                }else{
-                    element += temp;
-                }                
-                tempLast = "";
-            }else{
-                if(temp === "/" || temp === "x") tempLast = temp;
-                if(element) convertedArray.push(parseFloat(element.replace(",",".")));
-                element ="";
-                if(temp === "-" && tempLast !== ""){
-                    convertedArray.pop();
-                    temp = tempLast + temp;
-                    tempLast = "";
-                }
-                convertedArray.push(temp);
-            }
-        }
-        if(element !== "")convertedArray.push(parseFloat(element.replace(",",".")));
-        element ="";
-        if(first !== "")convertedArray[0] = convertedArray[0] * (-1);
-        console.log(convertedArray);
-        // Make calculation
-            let counter = convertedArray.length
-            let newElement;
-            for(let i = 0; i < convertedArray.length; i++){
-                if(convertedArray[i] == "x" || convertedArray[i] == "/" 
-                || convertedArray[i] == "x-" || convertedArray[i] == "/-"){                    
-                    newElement =  countOperation(convertedArray[i-1], convertedArray[i], convertedArray[i+1])
-                    convertedArray.splice(i,2);
-                    convertedArray[i-1] = newElement;
-                    counter = counter-2;
-                    i--;
-                    console.log(convertedArray)
-                }
-                if(i == counter )break ;
-            }
-            if(convertedArray.length > 1){
-                while(convertedArray.length > 1){
-                   newElement = countOperation(convertedArray.shift(), convertedArray.shift(), convertedArray.shift());
-                    convertedArray.unshift(newElement);
-                    console.log(convertedArray)
-                }                
-            }
-            
-            finalResult = convertedArray[0].toFixed(2);
-        
-        function countOperation(first, operator, second){
-            let res = 0 
-            console.log(operator)
-            console.log(first)
-            console.log(second)
-            switch(operator){
-                case "+": res = (first * 10 + second * 10) / 10;
-                break;
-                case "-": res = (first * 10 - second * 10) / 10;
-                break;
-                case "x": res = first * second;
-                break;
-                case "/": res = first / second;
-                break;
-                case "x-": res = -1 * first * second;
-                break;
-                case "/-": res =-1 * first / second;
-                break;
-                default: res = 0;
-                    break;
-            }
-            console.log(res)
-            return res;
-        }
-
-            
-        
-        memory = finalResult;
+    function displayResult() {        
+        memory = calculate().toString();
         btn = memory;
         noDots = true;
-        displayMemory.innerHTML = inputField.value
+        displayMemory.innerHTML = inputField.value;
+        /*let dis = memory.toString().replace(".", ",")
+        inputField.value = dis;*/
         inputField.value = memory;
     }
 
-    /**End Calculate Equal btn */
-
-    function isCharacterNumber(value){
-        return (value*1 || value === "0" )? true: false;
-    }    
+    /**End Calculate Equal btn */        
 
     inputField.value = memory;
     lastCharacter = btn;
